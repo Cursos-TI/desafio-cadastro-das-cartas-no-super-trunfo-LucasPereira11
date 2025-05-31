11651b80c53d21979f2b4383b75870124bcf2a7f
LucasPereira11 <lucasfpereira0121@gmail.com>
2025-05-31 01:31:32 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,43 +1,69 @@
 #include <stdio.h>
 
 int main(){
-    //Declaração de variavel da primeira carta
-    char estado1;
-    char codigo1[3];
-    char cidade1[50];
+    // Declaração das variáveis para a primeira carta
+    char estado1, 
+    char codigo1[4], 
+    char nomecidade1[50];
     int populacao1;
-    float area1;
+    float area1; 
     float pib1;
     int pontosturisticos1;
 
-//Carta 1
-printf("Digite o nome do estado1: \n");
-scanf("%c" , &estado1);
+    // Declaração das variáveis para a segunda carta
+    char estado2;
+    char codigo2[1];
+    char nomecidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
 
-printf("Digite o codigo1: \n");
-scanf("%s" , &codigo1);
+    //Carta 1
 
-printf("Digite o nome da cidade1: \n" );
-scanf("%s" , &cidade1);
- 
-printf("Digite o numero de habitantes1: \n");
-scanf ("%d" , &populacao1);
+    printf("Digite o Estado (A-H) da carta: \n");
+    scanf("%c", &estado1);
 
-printf("Digite a area em quilômetros quadrados1: \n");
-scanf("%f" , &area1);
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo1);
 
-printf("Digite o Produto Interno Bruto(pib)1: \n");
-scanf("%f" , &pib1);
+    print("Digite o nome da cidade: \n");
+    scanf("%s", &nomecidade1);
 
-printf("Digite o numero de pontos turisticos1: \n");
-scanf("%d" , &pontosturisticos1);
+    print("Digite o número de abitantes: \n");
+    scanf("%d", &populacao1);
 
-/imprimindo carta 1
-printf("Nome do estado: %c\n" , estado1);
-printf("codigo: %s\n" , codigo1);
-printf("Nome da cidade: %s\n" , cidade1);
-printf("Numero de habitantes :%d\n" , populacao1);
-printf("Area em quilômetros quadrados :%f\n" , area1);
-printf("Produto Interno Bruto(pib): %f\n" , pib1);
-printf("numero de pontos turisticos: %d\n" , pontosturisticos1);
+    printf("Digite a área em quilômetros quadrados: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o Produto Interno Bruto(PIB): \n");
+    scanf("%f", &pib1);
+
+    print("Pontos turísticos: \n");
+    scanf("%d", &pontosturisticos1);
+
+    //Carta 2
+
+    printf("Digite o Estado (A-H) da carta: \n");
+    scanf("%c", &estado2);
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo2);
+
+    print("Digite o nome da cidade: \n");
+    scanf("%s", &nomecidade2);
+
+    print("Digite o número de abitantes: \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a área em quilômetros quadrados: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o Produto Interno Bruto(PIB): \n");
+    scanf("%f", &pib2);
+
+    print("Pontos turísticos: \n");
+    scanf("%d", &pontosturisticos2);
+
+    return 0;
 }
